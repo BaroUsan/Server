d0e48433a4b1c5686c672220a9ac36b06e359780
huise0ng <shinhuiseong07@gmail.com>
2024-12-28 14:17:14 +0900

    ADD :: bssm.hs.kr 이메일만 회원가입 가능하도록 검증 절차 추가

--- src/signup/signup.service.ts
+++ src/signup/signup.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { SignupDto } from './dto/signup.dto';
@@ -11,9 +11,14 @@ export class SignupService {
   ) {}
 
   async signup(signupDto: SignupDto) {
+    const emailDomain = signupDto.email.split('@')[1];
+    if (emailDomain !== 'bssm.hs.kr') {
+      throw new BadRequestException('bssm.hs.kr 학교 이메일만 회원가입이 가능합니다.'); 
+    }
+
     const existingUser = await this.userModel.findOne({ email: signupDto.email });
     if (existingUser) {
-      throw new ConflictException('이미 사용 중인 이메일입니다.'); 
+      throw new ConflictException('이미 사용 중인 이메일입니다.');
     }
 
     const createdUser = new this.userModel(signupDto);
