d24756c82f3ce84c8a643a5d42c3ef509a1a4073
huise0ng <shinhuiseong07@gmail.com>
2024-12-28 17:39:47 +0900

    DDUNG :: 태그 명 변경

--- src/umb/umb.controller.ts
+++ src/umb/umb.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { UmbService } from './umb.service';
+import { ApiTags, ApiResponse } from '@nestjs/swagger'; 
 
+@ApiTags('우산 여부') 
 @Controller('umb')
 export class UmbController {
   constructor(private readonly umbService: UmbService) {}
 
   @Get('status')
+  @ApiResponse({ status: 200, description: 'Retrieve umbrella status.' }) 
   async getStatus() {
     return this.umbService.getStatus();
   }
