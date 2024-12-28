a2fb08f651d20e88895abe8a0a7f6e9279170df6
huise0ng <shinhuiseong07@gmail.com>
2024-12-28 13:43:20 +0900

    DOCS :: Swgger UI 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('바로우산 API')
+    .setDescription('바로우산은 부산소프트웨어마이스터고등학교 우산 대여 플랫폼입니다.')
+    .setVersion('0.9')
+    .addTag('인증/인가')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document); 
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
