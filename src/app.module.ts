114a22f70de0701a5f0882a3c5a1bc06cd7bcc91
huise0ng <shinhuiseong07@gmail.com>
2024-12-28 15:52:25 +0900

    FIX :: 요청 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SignupModule } from './signup/signup.module';
 import { SigninModule } from './signin/signin.module';
 import { AuthModule } from './auth/auth.module'; 
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { ConfigModule } from '@nestjs/config'; 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), 
     MongooseModule.forRoot(process.env.MONGODB_URI),
     SignupModule,
     SigninModule,
