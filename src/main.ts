f4ab2d4dbfed6557aea2ffb09a8f8df8df48205a
siniseong <shinhuiseong07@gmail.com>
2025-01-02 13:33:19 +0900

    ADD :: CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,20 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', 
+    allowedHeaders: 'Content-Type, Authorization', 
+    credentials: true, 
+  });
+
   const config = new DocumentBuilder()
     .setTitle('바로우산 API')
     .setDescription('바로우산은 부산소프트웨어마이스터고등학교 우산 대여 플랫폼입니다.')
     .setVersion('0.9')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/', app, document); 
+  SwaggerModule.setup('/', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
