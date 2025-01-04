27cb56cb6c6f0a3fc4c75d016742ffa930c33a68
신희성(Tori) <128358820+siniseong@users.noreply.github.com>
2025-01-04 15:01:51 +0900

    ADD :: CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:3000', 
-    methods: 'GET,POST,PUT,DELETE,OPTIONS', 
-    allowedHeaders: 'Content-Type, Authorization', 
-    credentials: true, 
-  });
+app.enableCors({
+  origin: ['http://localhost:3000', 'https://localhost:7114'], 
+  methods: 'GET,POST,PUT,DELETE,OPTIONS', 
+  allowedHeaders: 'Content-Type, Authorization', 
+  credentials: true, 
+});
+
 
   const config = new DocumentBuilder()
     .setTitle('바로우산 API')
